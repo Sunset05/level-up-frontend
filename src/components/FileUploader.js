a3aec719b1d4cd609a347c3328c0c81c7f6846d1
Keith Funk <Keith.C.Funk@gmail.com>
2021-07-21 12:08:18 -0600

    FileUploader component created

---
+++ src/components/FileUploader.js
@@ -0,0 +1,20 @@
+import React, { useRef } from 'react'
+
+export default function FileUploader({onFileSelectError, onFileSelectSuccess}) {
+    const fileInput = useRef(null);
+
+    const handleFileInput = (event) => {
+        const file = event.target.files[0];
+        if (file.size > 10024)
+            onFileSelectError(console.log(file));
+        else onFileSelectSuccess(file);
+    }
+
+    return (
+        <div className="file-uploader">
+            <input type="file" onChange={handleFileInput} />
+            <button onClick={(event) => {fileInput.current && fileInput.current.click()}} className="btn-primary"></button>
+        </div>
+    )
+}
+git 
